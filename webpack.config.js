db54c229fcae3935354a5b5fd34bc25305d8ab85
Santiago Ruiz <santiago.ruiz@bewe.io>
2021-02-12 09:03:39 -0500

    css with sass

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path')
+
+module.exports = {
+    entry: {
+        one: './css/one/styles.scss',
+        two: './css/two/styles.scss',
+        three: './css/three/styles.scss',
+        four: './css/four/styles.scss',
+        five: './css/five/styles.scss',
+    },
+    output: {
+        path: path.resolve(__dirname, "css"),
+        filename: '[name]/app.js',
+    },
+    plugins: [
+        new MiniCSSExtractPlugin({
+            filename: "[name]/styles.css",
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                loader: [
+                    MiniCSSExtractPlugin.loader,
+                    "css-loader",
+                    "sass-loader"
+                ]
+            }
+        ]
+    },
+    devServer: {
+        hot: true,
+    },
+}
